c0e95d8f495a34f382b55f9947593e0989882508
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-03-19 19:26:48 -0500

    created class calculadora

---
+++ ConsoleApp1/ConsoleApp1/Calc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Calc
+    {
+
+        public void ExecuteCalc()
+        {
+            float a = 0;
+            float b = 0;
+
+            Console.WriteLine("Ingrese el primer numero");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el primer numero");
+            b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la operacion : ");
+            string = Console.ReadLine();
+
+            
+
+
+        }
+
+    }
+}
